a7e962cabe76d8696601f4a14f65e1c356fea1f7
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2021-02-21 17:37:23 +0900

    new

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,28 @@
+class MessagesController < ApplicationController
+  def index
+    @message = Message.new
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+  def create
+    Message.create(message_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    if @message.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text,:image).merge(user_id: current_user.id)
+  end
+end
