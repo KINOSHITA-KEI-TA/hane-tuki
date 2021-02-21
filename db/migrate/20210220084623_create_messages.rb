cdf937b6f46c7b94bc53b41a220120cf128e9df2
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2021-02-21 18:15:25 +0900

    null 制約

--- db/migrate/20210220084623_create_messages.rb
+++ db/migrate/20210220084623_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.integer :user_id
-      t.text :image
-      t.text :text
+      t.text :image, null: false
+      t.text :text, null: false
       t.timestamps
     end
   end
